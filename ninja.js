b5ce0e8f10a97f6a1e2581530b2db63a16e6af97
tschulzv <139179840+tschulzv@users.noreply.github.com>
2024-02-25 14:53:31 -0300

    [TAREA] sensei

--- ninja.js
+++ ninja.js
@@ -2,9 +2,9 @@
 
 class Ninja {
     // ninja with 10 health as default
-    constructor(name, health = 10){
+    constructor(name){
         this.name = name;
-        this.health = health;
+        this.health = 10;
         this.speed = 3;
         this.strength = 3;
     }
@@ -23,11 +23,29 @@ class Ninja {
     }
 }
 
+class Sensei extends Ninja {
+    constructor(name){
+        super(name);
+        this.health = 200;
+        this.speed = 10; 
+        this.strength = 10;
+        this.wisdom = 10;
+    }
+
+    speakWisdom(){
+        super.drinkSake();
+        console.log("\"Lo que un programador puede hacer en un mes, dos programadores pueden hacerlo en dos meses.\"");
+    }
+}
+
 const ninja1 = new Ninja("Hyabusa");
 ninja1.sayName();
 ninja1.showStats();
 ninja1.drinkSake();
 ninja1.showStats();
 
-const ninja2 = new Ninja("Ninja2", 30);
-ninja2.showStats();
+// ejemplo de salida
+const superSensei = new Sensei("Master Splinter");
+superSensei.speakWisdom();
+superSensei.showStats();
+// -> "Nombre: Master Splinter, Salud: 210, Velocidad: 10, Fuerza: 10"
